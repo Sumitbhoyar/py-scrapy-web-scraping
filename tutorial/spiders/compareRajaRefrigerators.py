cf383b3045bb29f378a495966276115a5341cb05
Sumit Bhoyar <sumitbhoyar@gmail.com>
2016-11-12 15:02:55 +0530

    Price minimum price

--- tutorial/spiders/compareRajaRefrigerators.py
+++ tutorial/spiders/compareRajaRefrigerators.py
@@ -1,6 +1,4 @@
 import scrapy
-from scrapy import Request
-
 
 class AuthorSpider(scrapy.Spider):
     name = 'freeze'
@@ -9,28 +7,24 @@ class AuthorSpider(scrapy.Spider):
     start_urls = [
         'https://www.compareraja.in/filter/controlls/commonfinderext.aspx?page=1&&Type=&Blower/Fan=&minPrice=2300&maxPrice=58000&categoryId=53&CategoryNameInURLs=air-coolers&p_csv_Filter_01_Ids=&p_csv_Filter_02_Ids=&p_csv_Filter_03_Ids=&p_csv_Filter_04_Ids=&p_csv_Filter_05_Ids=&catid=53&catname=air-coolers',
     ]
-
+    price = 0
     def parse(self, response):
         self.counter += 1
+
         # follow links to author pages
         products = response.css('article.product')
-        print('calling 1')
         if len(products) == 0:
-            print('its final ')
+            print('its final link')
             return
         for product in products:
-            print('called')
-            yield scrapy.Request(response.urljoin(product.css('a::attr(href)').extract_first()),
-                                  callback=self.parse_productDetails)
-
-
+            self.price = product.css('b::text').extract_first().encode('utf-8').strip()
+            yield scrapy.Request(response.urljoin(product.css('a::attr(href)').
+                    extract_first()), callback=self.parse_productDetails)
         # follow pagination links
         next_page = self.url % self.counter
-        print('next page=' + str(next_page))
         yield scrapy.Request(next_page, callback=self.parse)
 
     def parse_productDetails(self, response):
-        return
         def extract_with_css(query):
             return response.css(query).extract_first().strip()
 
@@ -44,6 +38,7 @@ class AuthorSpider(scrapy.Spider):
             'reviews': reviews,
             'productDetails': str(productDetails),
             'image_urls': response.css('a.simpleLens-thumbnail-wrapper img::attr(src)').extract(),
+            'price': self.price,
         }
 
     def getProductDetails(self, response):
@@ -74,12 +69,12 @@ class AuthorSpider(scrapy.Spider):
             # element = priceElements[count]
             url = element.css('li.cp-c6 a::attr(onclick)').extract_first().encode('utf-8').strip()
             # url = str(urls[count])
-            rating = element.css('li.cp-c2 img::attr(src)').extract_first().encode('utf-8').strip()
+            # rating = element.css('li.cp-c2 img::attr(src)').extract_first().encode('utf-8').strip()
             stores[url.split(',', 4)[3].replace("'", '')] = store
             url = url[url.find('(') + 2:]
             url = url[:url.find("'")]
             store['url'] = url
-            store['rating']=rating
+            # store['rating']=rating
 
         return stores
 
